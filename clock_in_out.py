e387dd1a08c8929036c3d4b85c89a9cbe2e65bf7
waseem6670 <waseem6670@gmail.com>
2024-08-13 23:05:11 +0530

    Update clock_in_out.py

--- clock_in_out.py
+++ clock_in_out.py
@@ -1,25 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
 import os
 
 def clock_in_or_out():
     options = Options()
-    # Comment out the headless option to see the browser window
+    # Uncomment if you want to run in headless mode
     # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-setuid-sandbox')
-    options.add_argument('--remote-debugging-port=9222')
     options.add_argument('--window-size=1920x1080')
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    service = FirefoxService(GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service, options=options)
 
     try:
         driver.get('https://rochem.darwinbox.in/user/login')
@@ -35,7 +32,7 @@ def clock_in_or_out():
 
         # Find the clock button in the header and click it
         clock_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//header//img[@src="/ms/dboxuilibrary/assets/dboxuilib_dist/www/assets/images/Clock.svg"]')))
-        
+       
 
         print("Clock-in/out successful.")
     except Exception as e:
