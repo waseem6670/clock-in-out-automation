64e992a04a3dd8a6e05ec01ce328f8f579401961
waseem6670 <waseem6670@gmail.com>
2024-08-13 22:54:44 +0530

    Update clock_in_out.py

--- clock_in_out.py
+++ clock_in_out.py
@@ -1,4 +1,3 @@
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -6,10 +5,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 def clock_in_or_out():
     options = Options()
-    options.add_argument('--headless')
+    # Comment out the headless option to see the browser window
+    # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
@@ -32,7 +33,7 @@ def clock_in_or_out():
 
         # Find the clock button in the header and click it
         clock_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//header//img[@src="/ms/dboxuilibrary/assets/dboxuilib_dist/www/assets/images/Clock.svg"]')))
-        clock_button.click()
+      
 
         print("Clock-in/out successful.")
     except Exception as e:
