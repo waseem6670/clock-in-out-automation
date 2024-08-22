f7b493108258ba2ae2ff9a0ef04924430c73979f
waseem6670 <waseem6670@gmail.com>
2024-08-23 00:25:12 +0530

    Update clock_in_out.py

--- clock_in_out.py
+++ clock_in_out.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options.Options
+from selenium.webdriver.chrome.options import Options  # Corrected import statement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
