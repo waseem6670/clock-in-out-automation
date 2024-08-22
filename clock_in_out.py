0a456f8bd4225c41eec747fec951f0f746937156
waseem6670 <waseem6670@gmail.com>
2024-08-23 00:23:02 +0530

    Update clock_in_out.py

--- clock_in_out.py
+++ clock_in_out.py
@@ -1,11 +1,12 @@
 import os
 from datetime import datetime, timedelta
 import logging
+import time  # Importing the time module
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options.Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
